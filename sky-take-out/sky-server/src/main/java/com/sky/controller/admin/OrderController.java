da31ede059a5502a42f64c990effe74d384e02aa
tuanzi <lvbohan2003@163.com>
2025-07-03 02:55:10 +0800

    管理端查询订单详情

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -4,10 +4,12 @@ import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,6 +44,18 @@ public class OrderController {
     public Result<Map<String, Integer>> statistics() {
         Map<String, Integer> map = orderService.statistics();
         return Result.success(map);
+    }
+
+    /**
+     * 查看各订单的详情
+     * @param id
+     * @return
+     */
+    @GetMapping("/details/{id}")
+    @ApiOperation("查看各订单的详情")
+    public Result<OrderVO> getOrderDetails(@PathVariable Long id) {
+        OrderVO orderVO = orderService.getOrderDetailById(id);
+        return Result.success(orderVO);
     }
 
 }
